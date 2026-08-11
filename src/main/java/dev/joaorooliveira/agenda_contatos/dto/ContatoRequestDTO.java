e05f0ed5ae7b72c96309b2b195b3be2ffb2c1d9c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-10 23:42:11 -0300

    feat: adiciona contato request dto

--- src/main/java/dev/joaorooliveira/agenda_contatos/dto/ContatoRequestDTO.java
+++ src/main/java/dev/joaorooliveira/agenda_contatos/dto/ContatoRequestDTO.java
@@ -1,4 +1,28 @@
 package dev.joaorooliveira.agenda_contatos.dto;
 
-public record ContatoRequestDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ContatoRequestDTO(
+
+        @NotBlank(message = "O nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "O e-mail deve ser válido")
+        String email,
+
+        @NotBlank(message = "O telefone é obrigatório")
+        @Size(
+                min = 8,
+                max = 20,
+                message = "O telefone deve ter entre 8 e 20 caracteres"
+        )
+        String telefone,
+
+        String observacoes,
+
+        boolean favorito
+) {
 }
