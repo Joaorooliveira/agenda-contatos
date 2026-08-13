f42bcf594e0a92c0e3359d8ef59906708b5ce98f
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-12 22:01:53 -0300

    feat(contato-service): adiciona metodo para salvar contato

--- src/main/java/dev/joaorooliveira/agenda_contatos/service/ContatoService.java
+++ src/main/java/dev/joaorooliveira/agenda_contatos/service/ContatoService.java
@@ -1,4 +1,28 @@
 package dev.joaorooliveira.agenda_contatos.service;
 
+import dev.joaorooliveira.agenda_contatos.dto.ContatoRequestDTO;
+import dev.joaorooliveira.agenda_contatos.repository.ContatoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class ContatoService {
+
+    private final ContatoRepository contatoRepository;
+
+    public ContatoService(ContatoRepository contatoRepository) {
+        this.contatoRepository = contatoRepository;
+    }
+
+    @Transactional
+    public void salvarContato(ContatoRequestDTO contatoRequestDTO) {
+        var contato = contatoRequestDTO.toEntity();
+        contatoRepository.save(contato);
+    }
+
+
+
+
+
+
 }
