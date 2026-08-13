f4e95c6758ccb31851547cf98ba7fa09c9713fb5
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-12 22:52:36 -0300

    feat(contato-service): adiciona metodo para listar contatos

--- src/main/java/dev/joaorooliveira/agenda_contatos/service/ContatoService.java
+++ src/main/java/dev/joaorooliveira/agenda_contatos/service/ContatoService.java
@@ -3,6 +3,8 @@ package dev.joaorooliveira.agenda_contatos.service;
 import dev.joaorooliveira.agenda_contatos.dto.ContatoRequestDTO;
 import dev.joaorooliveira.agenda_contatos.dto.ContatoResponseDTO;
 import dev.joaorooliveira.agenda_contatos.repository.ContatoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,15 +23,17 @@ public class ContatoService {
         contatoRepository.save(contato);
     }
 
-//    public Page<ContatoResponseDTO> listarContatos(Pageable pageable) {
-//
-//    }
+    public Page<ContatoResponseDTO> listarContatos(Pageable pageable) {
+        return contatoRepository.findAll(pageable).map(ContatoResponseDTO::fromEntity);
+    }
 
     public ContatoResponseDTO listarContato(Long id) {
         var contato = contatoRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Contato não encontrado"));
         return ContatoResponseDTO.fromEntity(contato);
     }
+
+
 
 
 
