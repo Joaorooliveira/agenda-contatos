384d39edbf1546c95ffc637d59170bef2ff866f4
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-12 22:13:36 -0300

    feat(contato-response-dto): adiciona metodo para conversao de entidade para response dto

--- src/main/java/dev/joaorooliveira/agenda_contatos/dto/ContatoResponseDTO.java
+++ src/main/java/dev/joaorooliveira/agenda_contatos/dto/ContatoResponseDTO.java
@@ -1,5 +1,7 @@
 package dev.joaorooliveira.agenda_contatos.dto;
 
+import dev.joaorooliveira.agenda_contatos.domain.Contato;
+
 public record ContatoResponseDTO(
         Long id,
         String nome,
@@ -8,4 +10,14 @@ public record ContatoResponseDTO(
         String observacoes,
         boolean favorito
 ) {
+    public static ContatoResponseDTO fromEntity(Contato contato) {
+        return new ContatoResponseDTO(
+                contato.getId(),
+                contato.getNome(),
+                contato.getEmail(),
+                contato.getTelefone(),
+                contato.getObservacoes(),
+                contato.isFavorito()
+        );
+    }
 }
