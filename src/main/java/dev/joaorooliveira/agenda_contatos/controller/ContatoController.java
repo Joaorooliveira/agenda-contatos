09fce8581e7f6fec775587a71f284154f116bf3d
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-13 22:54:42 -0300

    feat(contato-controller): adiciona endpoint para buscar contato pelo id

--- src/main/java/dev/joaorooliveira/agenda_contatos/controller/ContatoController.java
+++ src/main/java/dev/joaorooliveira/agenda_contatos/controller/ContatoController.java
@@ -4,6 +4,9 @@ import dev.joaorooliveira.agenda_contatos.dto.ContatoRequestDTO;
 import dev.joaorooliveira.agenda_contatos.dto.ContatoResponseDTO;
 import dev.joaorooliveira.agenda_contatos.service.ContatoService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,4 +38,20 @@ public class ContatoController {
 
         return ResponseEntity.created(location).body(contato);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ContatoResponseDTO>> listar(
+            @PageableDefault(size = 10, sort = "nome") Pageable pageable) {
+
+        return ResponseEntity.ok(
+                contatoService.listarContatos(pageable)
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContatoResponseDTO> listarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                contatoService.listarContatoPorId(id)
+        );
+}
 }
