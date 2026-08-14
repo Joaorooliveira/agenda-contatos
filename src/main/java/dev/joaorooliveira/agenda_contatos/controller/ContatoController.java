c50b8b1b3019c7cf0b909617f7afdb6db6063679
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-13 22:17:44 -0300

    feat(contato-controller): adiciona endpoint para salvar contato

--- src/main/java/dev/joaorooliveira/agenda_contatos/controller/ContatoController.java
+++ src/main/java/dev/joaorooliveira/agenda_contatos/controller/ContatoController.java
@@ -1,9 +1,38 @@
 package dev.joaorooliveira.agenda_contatos.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.joaorooliveira.agenda_contatos.dto.ContatoRequestDTO;
+import dev.joaorooliveira.agenda_contatos.dto.ContatoResponseDTO;
+import dev.joaorooliveira.agenda_contatos.service.ContatoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/contatos")
 public class ContatoController {
+
+    private final ContatoService contatoService;
+
+    public ContatoController(ContatoService contatoService) {
+        this.contatoService = contatoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ContatoResponseDTO> salvar(
+            @RequestBody @Valid ContatoRequestDTO dto) {
+
+        ContatoResponseDTO contato = contatoService.salvarContato(dto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(contato.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(contato);
+    }
 }
