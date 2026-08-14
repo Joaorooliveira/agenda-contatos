88ef12da58bdfc0ac79591a8c2ddb07dc7134867
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-13 21:26:55 -0300

    feat: adiciona contato controller

---
+++ src/main/java/dev/joaorooliveira/agenda_contatos/controller/ContatoController.java
@@ -0,0 +1,9 @@
+package dev.joaorooliveira.agenda_contatos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contatos")
+public class ContatoController {
+}
