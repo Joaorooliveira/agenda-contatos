29bd88d503728fd65f6601189888d457c6a732e1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-08-14 23:01:50 -0300

    feat(contato-repository): adiciona specification

--- src/main/java/dev/joaorooliveira/agenda_contatos/repository/ContatoRepository.java
+++ src/main/java/dev/joaorooliveira/agenda_contatos/repository/ContatoRepository.java
@@ -2,6 +2,7 @@ package dev.joaorooliveira.agenda_contatos.repository;
 
 import dev.joaorooliveira.agenda_contatos.domain.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ContatoRepository extends JpaRepository<Contato, Long> {
+public interface ContatoRepository extends JpaRepository<Contato, Long>, JpaSpecificationExecutor<Contato> {
 }
